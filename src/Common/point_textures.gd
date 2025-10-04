6d9fa98efee02f58ea95ceb3d1fec90bfffd76c2
Szymii <szymimelzer9@gmail.com>
2025-10-04 22:14:31 +0200

    New textures

--- src/Common/point_textures.gd
+++ src/Common/point_textures.gd
@@ -13,6 +13,13 @@ var textures: Array[PointTextureData] = [
 	PointTextureData.new("res://assets/tree/point_textures/Mana.png", "Mana"),
 	PointTextureData.new("res://assets/tree/point_textures/LifeAndMana.png", "LifeAndMana"),
 	PointTextureData.new("res://assets/tree/point_textures/Cold.png", "Cold"),
+	PointTextureData.new("res://assets/tree/point_textures/Fire.png", "Fire"),
+	PointTextureData.new("res://assets/tree/point_textures/Lightning.png", "Lightning"),
+	PointTextureData.new("res://assets/tree/point_textures/Elemental.png", "Elemental"),
+	PointTextureData.new("res://assets/tree/point_textures/Phys.png", "Phys"),
+	PointTextureData.new("res://assets/tree/point_textures/2HandAxe.png", "2HandAxe"),
+	PointTextureData.new("res://assets/tree/point_textures/1HandAxe.png", "1HandAxe"),
+	PointTextureData.new("res://assets/tree/point_textures/Shield.png", "Shield"),
 	# Movement
 	PointTextureData.new("res://assets/tree/point_textures/Dash.png", "Dash"),
 	PointTextureData.new("res://assets/tree/point_textures/Teleport.png", "Teleport"),
