f9b5d1eb171d4bbb286c7b0ca27fb86b320f43e5
Szymii <szymimelzer9@gmail.com>
2025-10-09 18:27:16 +0200

    Scene manager

--- src/game_controller.gd
+++ src/game_controller.gd
@@ -4,8 +4,41 @@ extends Node
 @export var world: Node2D
 @export var gui: Control
 
-var current_word_scene: Node2D
+var current_world_scene: Node2D
 var current_gui_scene: Control
 
 func _ready() -> void:
 	Global.game_controller = self
+	current_gui_scene = %MainMenu
+
+func change_world_scene(new_scene: String) -> void:
+	if current_world_scene:
+		world.remove_child(current_world_scene)
+		current_world_scene.queue_free()
+	
+	var packed_scene: PackedScene = load(new_scene) as PackedScene
+	var scene: Node = packed_scene.instantiate()
+	world.add_child(scene)
+	current_world_scene = scene
+
+func change_gui_scene(new_scene: String) -> void:
+	if current_gui_scene:
+		gui.remove_child(current_gui_scene)
+		current_gui_scene.queue_free()
+	
+	var packed_scene: PackedScene = load(new_scene) as PackedScene
+	var scene: Control = packed_scene.instantiate() as Control
+	gui.add_child(scene)
+	current_gui_scene = scene
+
+func clear_world_scene() -> void:
+	if current_world_scene:
+		world.remove_child(current_world_scene)
+		current_world_scene.queue_free()
+		current_world_scene = null
+
+func clear_gui_scene() -> void:
+	if current_gui_scene:
+		gui.remove_child(current_gui_scene)
+		current_gui_scene.queue_free()
+		current_gui_scene = null
--- src/main_menu.gd
+++ src/main_menu.gd
@@ -8,10 +8,8 @@ func _ready() -> void:
 	start_btn.pressed.connect(_on_start_pressed)
 	exit_btn.pressed.connect(_on_exit_pressed)
 
-
 func _on_start_pressed() -> void:
-	print("start")
-
+	Global.game_controller.change_gui_scene("res://src/Systems/CharacterSelection/CharacterSelection.tscn")
 
 func _on_exit_pressed() -> void:
 	get_tree().quit()
