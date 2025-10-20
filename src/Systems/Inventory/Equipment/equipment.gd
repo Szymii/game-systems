f31a200a131580be0236fb000bcada103bf7c59f
Szymii <szymimelzer9@gmail.com>
2025-10-20 20:06:26 +0200

    Revorkerd equipment

--- src/Systems/Inventory/Equipment/equipment.gd
+++ src/Systems/Inventory/Equipment/equipment.gd
@@ -15,55 +15,61 @@ extends MarginContainer
 @onready var belt: ColorRect = %Belt
 
 var drag_manager: DragDropManager
-
-var _slots: Dictionary = {}
+var _slots: Array[ColorRect] = []
+var _equipped_items: Dictionary = {}
+var _item_views: Dictionary = {}
 
 func initialize(_drag_manager: DragDropManager) -> void:
 	drag_manager = _drag_manager
-	
 	_setup_slots()
 
 func _setup_slots() -> void:
-	_register_slot(weapon_slot, ItemTags.ITEM_TAGS.WEAPON)
-	_register_slot(weapon_shield_slot, ItemTags.ITEM_TAGS.SHIELD)
-	_register_slot(helmet, ItemTags.ITEM_TAGS.HELMET)
-	_register_slot(body_armour, ItemTags.ITEM_TAGS.BODY_ARMOUR)
-	_register_slot(gloves, ItemTags.ITEM_TAGS.GLOVES)
-	_register_slot(boots, ItemTags.ITEM_TAGS.BOOTS)
-	_register_slot(necklace, ItemTags.ITEM_TAGS.NECKLACE)
-	_register_slot(earring, ItemTags.ITEM_TAGS.EARRING)
-	_register_slot(ring_right, ItemTags.ITEM_TAGS.RING)
-	_register_slot(belt, ItemTags.ITEM_TAGS.BELT)
-
-func _register_slot(slot_node: ColorRect, slot_type: ItemTags.ITEM_TAGS) -> void:
-	_slots[slot_type] = slot_node
-	slot_node.gui_input.connect(func(_event: InputEvent) -> void: _on_slot_input(slot_type))
-
-func _on_slot_input(slot_type: ItemTags.ITEM_TAGS) -> void:
-	if Input.is_action_just_pressed("interaction"):
-		_handle_slot_click(slot_type)
-
-func _handle_slot_click(slot_type: ItemTags.ITEM_TAGS) -> void:
-	var held_item := drag_manager.get_held_item()
+	_slots = [weapon_slot, earring, body_armour, gloves, boots,
+			  weapon_shield_slot, helmet, necklace, ring_right, belt]
 	
-	if held_item:
-		_try_equip_held_item(slot_type)
+	for slot in _slots:
+		slot.gui_input.connect(func(event: InputEvent) -> void: _on_slot_clicked(event, slot))
+
+func _on_slot_clicked(_event: InputEvent, slot: ColorRect) -> void:
+	if Input.is_action_just_pressed("interaction"):
+		_handle_slot_click(slot)
+
+func _handle_slot_click(slot: ColorRect) -> void:
+	var held_view := drag_manager.get_held_item()
+	
+	if held_view:
+		_equip_item(slot, held_view)
 	else:
-		_try_unequip_item(slot_type)
+		_unequip_item(slot)
 
-func _try_equip_held_item(slot_type: ItemTags.ITEM_TAGS) -> void:
-	var held_item := drag_manager.get_held_item()
+func _equip_item(slot: ColorRect, held_view: InventoryItemView) -> void:
+	var old_view: InventoryItemView = null
+	if _item_views.has(slot):
+		old_view = _item_views[slot]
+		_item_views.erase(slot)
+		_equipped_items.erase(slot)
+	
+	drag_manager.end_drag()
+	
+	var slot_center := slot.global_position + slot.size / 2
+	held_view.set_position_from_slot(slot_center - held_view.size / 2)
+	
+	_equipped_items[slot] = held_view.item_data
+	_item_views[slot] = held_view
+	
+	if old_view and is_instance_valid(old_view):
+		old_view.global_position = get_viewport().get_mouse_position()
+		drag_manager.start_drag(old_view)
 
-	# check if item can be equiped
-
-	# check if this slot has item
-
-	# save item in slot
-	# setup view
-	# free drag manager
-	# emit signal
-
-	pass
-
-func _try_unequip_item(slot_type: ItemTags.ITEM_TAGS) -> void:
-	pass
+func _unequip_item(slot: ColorRect) -> void:
+	if not _item_views.has(slot):
+		return
+	
+	var item_view: InventoryItemView = _item_views[slot]
+	if not item_view or not is_instance_valid(item_view):
+		return
+	
+	_item_views.erase(slot)
+	_equipped_items.erase(slot)
+	
+	drag_manager.start_drag(item_view)
