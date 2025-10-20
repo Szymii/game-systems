e0cbfd9f3877b7610370b115cbb351046b5ab868
Szymii <szymimelzer9@gmail.com>
2025-10-20 17:09:08 +0200

    border colour on items

--- src/Systems/Inventory/InventoryItem/inventory_item.gd
+++ src/Systems/Inventory/InventoryItem/inventory_item.gd
@@ -1,6 +1,8 @@
 class_name InventoryItemView
 extends Sprite2D
 
+@onready var border_container: PanelContainer = %BorderContainer
+
 var item_data: ItemData = null
 var slot_index: int = -1
 
@@ -17,14 +19,28 @@ var anchor_point: Vector2:
 func _ready() -> void:
 	if item_data:
 		texture = item_data.texture
+	_update_border_container()
 
 func set_item_data(_item_data: ItemData) -> void:
 	item_data = _item_data
 	if item_data:
 		texture = item_data.texture
+	_update_border_container()
 
 func set_position_from_slot(pos: Vector2) -> void:
 	global_position = pos + size / 2
+	_update_border_container()
 
 func update_visual_position(pos: Vector2) -> void:
 	global_position = pos
+
+func _update_border_container() -> void:
+	if not border_container or not item_data:
+		return
+	
+	var rarity_color: Color = Rarity.get_rarity_color(item_data.rarity)
+	border_container.self_modulate = rarity_color
+
+	var border_size := size - Vector2(2, 2)
+	border_container.size = border_size
+	border_container.position = - border_size / 2
